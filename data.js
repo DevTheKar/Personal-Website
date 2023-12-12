cea4e2336076156fb4c5e9a16bfb56f9a3c20870
devthekar <devthekar@gmail.com>
2023-12-12 00:25:21 -0500

    First Commit

---
+++ data.js
@@ -0,0 +1,22 @@
+const servicesData = [
+    {
+        title: "Frontend Development",
+        category: "frontend",
+        description: ['HTML5', 'CSS', 'JS (SwiperJS, MixItUp)', 'WordPress']
+    },
+    {
+        title: "Backend Development",
+        category: "backend",
+        description: ['Django', 'MySQL', 'API Integration']
+    },
+    {
+        title: "Machine Learning",
+        category: "machinelearning",
+        description: ['Python', 'Pytorch', 'Tensorflow', 'Roboflow', 'Yolov8 Learning Model']
+    },
+    {
+        title: "Game Development",
+        category: "gamedevelopment",
+        description: ['Godot (Python)', 'Unity (C#)']
+    }
+]
