483a8e8d9aa56459f7f1cad108129db127dbc80e
Dev Thakkar <devthekar@gmail.com>
2024-05-03 19:47:13 -0400

    Finalize files

---
+++ data.js
@@ -0,0 +1,27 @@
+const servicesData = [
+    {
+        title: "Frontend Development",
+        category: "frontend",
+        description: ['HTML5', 'CSS', 'JS (SwiperJS, MixItUp)', 'WordPress']
+    },
+    {
+        title: "Backend Development",
+        category: "backend",
+        description: ['Django', 'MySQL', 'API Integration']
+    },
+    {
+        title: "Machine Learning",
+        category: "machinelearning",
+        description: ['Python', 'Pytorch', 'Tensorflow', 'Roboflow', 'Yolov8 Learning Model', 'NEAT Learning Model']
+    },
+    {
+        title: "Game Development",
+        category: "gamedevelopment",
+        description: ['Godot (Python)', 'Unity (C#)', 'PyGame']
+    },
+    {
+        title: "Research/Clubs",
+        category: "research",
+        description: ['RANCS Researh Lab Assistant', 'Bulls Racing Data Acquisition Team']
+    }
+]
